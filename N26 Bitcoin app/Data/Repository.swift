81a4851d1cd132c1d44fd5bf0bf9112ed8b006b5
Dmytro Kopanytsia <mail@thedmytro.me>
2025-05-23 16:34:45 +0200

    Repository | Basic uni-directional data flow repo

--- N26 Bitcoin app/Data/Repository.swift
+++ N26 Bitcoin app/Data/Repository.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Combine
+
+// MARK: - Core Protocols
+protocol State {}
+
+protocol Action {}
+
+protocol Reducer {
+    associatedtype AppStateType: State
+    func reduce(state: AppStateType, action: Action) -> AppStateType
+}
+
+// MARK: - Repository
+@MainActor
+class Repository<AppStateType: State, AppReducerType: Reducer>: ObservableObject where AppReducerType.AppStateType == AppStateType {
+    @Published private(set) var state: AppStateType
+    
+    private let reducer: AppReducerType
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(initialState: AppStateType, reducer: AppReducerType) {
+        self.state = initialState
+        self.reducer = reducer
+    }
+    
+    func dispatch(_ action: Action) {
+        let newState = reducer.reduce(state: state, action: action)
+        state = newState
+    }
+}
+
+// MARK: - State Observing
+extension Repository {
+    func observe<T>(_ keyPath: KeyPath<AppStateType, T>) -> AnyPublisher<T, Never> where T: Equatable {
+        return $state
+            .map(keyPath)
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
+}
