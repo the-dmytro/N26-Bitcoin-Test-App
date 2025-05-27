5a2a63eb806b9100d84273467d04e672c20cf96e
Dmytro Kopanytsia <mail@thedmytro.me>
2025-05-27 18:33:01 +0200

    View | PriceDetailView
    + PriceDetailView with basic UI relying on existing views
    + PriceDetailViewModel

--- N26 Bitcoin app/Presentation/Scenes/PriceDetail/PriceDetailView.swift
+++ N26 Bitcoin app/Presentation/Scenes/PriceDetail/PriceDetailView.swift
@@ -5,4 +5,27 @@
 //  Created by Dmytro Kopanytsia on 19/5/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct PriceDetailView: View {
+    @StateObject private var viewModel: PriceDetailViewModel
+
+    init(viewModel: PriceDetailViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        List {
+            Section(header: Text(viewModel.date.formatted(Date.FormatStyle().day().month().year()))) {
+                PriceHistorySectionView(state: viewModel.priceLoadingState, retryAction: viewModel.retry)
+            }
+        }
+        .navigationTitle(String.navigationTitlePriceDetail)
+        .onAppear {
+            viewModel.onAppear()
+        }
+        .onDisappear {
+            viewModel.onDisappear()
+        }
+    }
+}
--- N26 Bitcoin app/Presentation/Scenes/PriceDetail/PriceDetailViewModel.swift
+++ N26 Bitcoin app/Presentation/Scenes/PriceDetail/PriceDetailViewModel.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+class PriceDetailViewModel: ObservableObject {
+    private let currencies: Set<Currency> = [.eur, .usd, .gbp]
+
+    private let repository: AppRepository
+    private let priceUseCase: DayPriceUseCase
+    private var cancellables: Set<AnyCancellable> = []
+
+    let date: Date
+
+    @Published var priceLoadingState: PriceLoadingState = .notLoaded
+
+    init(repository: AppRepository, priceUseCase: DayPriceUseCase, date: Date) {
+        self.repository = repository
+        self.priceUseCase = priceUseCase
+        self.date = date
+
+        setupSubscriptions()
+    }
+    
+    func onAppear() {
+        Task {
+            await priceUseCase.execute(input: .init(date: date, currencies: Array(currencies)))
+        }
+    }
+
+    func onDisappear() {
+        repository.dispatch(SelectedDayPriceAction.reset)
+    }
+
+    func retry() {
+        Task {
+            await priceUseCase.execute(input: .init(date: date, currencies: Array(currencies)))
+        }
+    }
+
+    private func setupSubscriptions() {
+        repository.observe(\.selectedDay)
+            .sink { [weak self] selectedDayState in
+                self?.updateSelectedDayState(date: selectedDayState.date, prices: selectedDayState.prices, loadingState: selectedDayState.loadingState)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func updateSelectedDayState(date: Date?, prices: [Price], loadingState: LoadingState) {
+        switch loadingState {
+        case .loading:
+            priceLoadingState = .loading
+        case .loaded:
+            let filteredPrices = prices.filter { currencies.contains($0.currency) }
+            priceLoadingState = .loaded(filteredPrices)
+        case .loadingError(let error):
+            priceLoadingState = .error(error)
+        default:
+            break
+        }
+    }
+}
